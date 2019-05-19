3e34362af2d435d4ec79850f7d66aa5ccc9c78c5
bi od <binodnp@outlook.com>
2019-05-20 00:40:30 +0545

    Fixed permission issue for cases where the SQL user is not the database owner.

--- db/SQL Server/purchase-2.1.update.sql
+++ db/SQL Server/purchase-2.1.update.sql
@@ -1752,10 +1752,16 @@ GO
 
 
 -->-->-- src/Frapid.Web/Areas/MixERP.Purchases/db/SQL Server/2.1.update/src/99.ownership.sql --<--<--
-EXEC sp_addrolemember  @rolename = 'db_owner', @membername  = 'frapid_db_user'
+IF(IS_ROLEMEMBER ('db_owner') = 1)
+BEGIN
+	EXEC sp_addrolemember  @rolename = 'db_owner', @membername  = 'frapid_db_user';
+END
 GO
 
-EXEC sp_addrolemember  @rolename = 'db_datareader', @membername  = 'report_user'
+IF(IS_ROLEMEMBER ('db_owner') = 1)
+BEGIN
+	EXEC sp_addrolemember  @rolename = 'db_datareader', @membername  = 'report_user'
+END
 GO
 
 DECLARE @proc sysname
